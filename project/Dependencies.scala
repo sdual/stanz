51504088286c80586a8a38ce1e4e2dafa8482759
stenoritama <shr.nrht.0623@gmail.com>
2018-07-28 14:03:04 +0900

    fix indent

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   val scalaTest         = "org.scalatest"       %% "scalatest"    % "3.0.5"
-  val apacheCommonsMath =  "org.apache.commons" % "commons-math3" % "3.6.1"
+  val apacheCommonsMath = "org.apache.commons" % "commons-math3" % "3.6.1"
 
   val stanzDependencies = Seq(apacheCommonsMath, scalaTest % Test)
 
