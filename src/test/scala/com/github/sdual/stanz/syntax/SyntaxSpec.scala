909db0728f1e1a3c0c73bba3ab7c79f7969d88e1
stenoritama <shr.nrht.0623@gmail.com>
2018-08-22 11:06:25 +0900

    wip

---
+++ src/test/scala/com/github/sdual/stanz/syntax/SyntaxSpec.scala
@@ -0,0 +1,8 @@
+package com.github.sdual.stanz.syntax
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{Matchers, WordSpec}
+
+class SyntaxSpec extends WordSpec with Matchers with MockFactory {
+
+}
