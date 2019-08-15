acfc8ae7cc363e8bb864c200255fa48e8479c2b1
sdual <shr.nrht.0623@gmail.com>
2019-08-16 02:49:30 +0900

    add pityka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,11 @@ object Dependencies {
   val scalaTest         = "org.scalatest"      %% "scalatest"    % "3.0.5"
   val scalaMock         = "org.scalamock"      %% "scalamock"    % "4.1.0"
   val apacheCommonsMath = "org.apache.commons" % "commons-math3" % "3.6.1"
+  val pityka            =  "io.github.pityka"  %% "nspl-core"    % "0.0.21"
 
   val stanzDependencies = Seq(
     apacheCommonsMath,
+    pityka,
     scalaTest % Test,
     scalaMock % Test
   )
